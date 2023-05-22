14811de617d28a6e2de9123624266096dd6b9f9b
Abdullah Esha <65135485+AbdullahEsha@users.noreply.github.com>
2023-05-23 01:52:16 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<head>
+	<title> What is MP3 </title>
+	<link href="styles/index.css" rel="stylesheet" />
+</head>
+
+<body>
+	<div class="header">
+		<h1> What is MP3 ? </h1>
+		<p> Created by Swinburne University Students </p>
+	</div>
+	<div class="navbar">
+		<a href="index.php">Index</a>
+		<a href="topic.php">Topic</a>
+		<a href="quiz.php">Quiz</a>
+		<a href="enhancements1.php">Enhancements </a>
+	</div>
+	<div class="row">
+		<div class="side">
+			<h2> MP3 </h2>
+			<p1>
+				<li> MP3 is a standard technology and compressed audio file format stands for "MPEG Audio Layer-3,
+					" which is developed by MPEG (Moving Picture Experts Group).
+					A file extension and compression method for compressing a sound sequence into a very small file
+					while
+					still retaining audio quality as comparad to a CD.
+					Commonly, MP3 files need less disk space and are used to hold a song or the whole CD data.</li><br>
+
+				<li> A computer can save many numbers of songs, even several hundred or thousands, as MP3 files require
+					very
+					small storage space.
+					There is a number of websites that allow users to download MP3 files, which are identified with the
+					file
+					name suffix of " mp3 ".</li>
+			</p1><br>
+
+			<img class="Mp3" src="images\cover.png" alt="image of a MP3" style="width:500px;height:500px;">
+			<br>
+			<br>
+			<br>
+			<br>
+			<h2> History </h2>
+
+			<img src="images/MP3-Team_1987-b.jpg" alt="founder of MP3" style="width:800px;height:500px;"><br><br>
+
+			<p1>
+				<li> In 1894, with the help of a tone of lower frequency, another tone could be rendered inaudible that
+					was
+					reported by the American physicist Alfred M. Mayer. Regarding this phenomenon, a complete set of
+					auditory curves was described by Richard Ehmer in 1959.
+
+					The German company Fraunhofer-Gesellschaft developed the MP3 and held the patent for MP3 technology
+					too.
+				</li><br><br>
+
+				<li> In 1987, research on high quality, low bit rate audio encoding was started by the Fraunhofer
+					Institute
+					Integrierte Schaltungen that was a research center and a part of Fraunhofer-Gesellschaft and gave
+					this
+					project name EUREKA EU147. At the University of Erlangen, Dieter Seitzer, when he was working on
+					transferring audio file information over a standard telephone line, helped with the coding.
+					Karlheinz
+					Brandenburg, originally known as the father of MP3 because his brain was the man behind the
+					development
+					of the MP3.</li><br><br>
+
+				<li> In 1972, a type of transform coding, the discrete cosine transformed (DCT), was proposed by Nasir
+					Ahmed
+					for lossy compression, and in 1973, Ahmed with T. Natarajan and K. R. Rao developed it. Also, its
+					result
+					was out by them in 1974.
+
+					Since 1977, he had been researching compression methods as he was an electronics specialist and
+					mathematics. An adaptive predictive coding, LPC speech codec was proposed by Manfred R. Schroeder
+					and
+					Bishnu S. Atal at Bell Labs in 1978, in which, to exploiting the masking properties of the human
+					ear,
+					using the psychoacoustic coding algorithm. Later in 1979, a paper was reported for further
+					optimization
+					by Atal and Schroeder. </li> <br><br>
+
+				<li> After this, in 1987, J. P. Princen, A. W. Johnson, and A. B. Bradley, by following the earlier In
+					April
+					1989, a patent to the technology was granted to Fraunhofer. Almost, the MP3 file format did not
+					exist,
+					according to Brandenburg. The encoding did not want to work in 1991 during the modification test.
+					But
+					they identified and solved the compiler error; till then, the first version of the MP3 codec was not
+					submitted. In 1999, to distribute their music as MP3s online, the first record label was the record
+					label Sub-Pop, which was one small relatively unknown fact in this year.</li><br><br>
+
+			</p1>
+
+			<img class="timeline" src="images\evolutionofthemp3.png" alt="history of MP3 timeline"
+				style="width:800px;height:500px;"><br><br>
+
+			<p1>
+				<li> In this website we will briefly learn about " What is MP3 ", history of MP3, Advantages &
+					Disadvantages
+					of MP3,
+					current and future status of MP3, similar technologies to MP3 and comparison between MP3 & MP4 </li>
+			</p1>
+
+			<div class="footer">
+				<p> Contact me at <a href="mailto:jlai@swin.edu.au">jlai@swin.edu.au</a></p>
+			</div>
+
+</body>
+
+</html>
