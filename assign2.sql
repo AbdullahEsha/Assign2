e01ad48761251f9768a2c990c76d0613b7a79975
AbdullahIsha <shahariaresha@gmail.com>
2023-05-30 18:44:29 +0600

    update

--- assign2.sql
+++ assign2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 28, 2023 at 10:05 PM
+-- Generation Time: May 30, 2023 at 02:44 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -29,19 +29,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `attempts` (
   `id` int(10) NOT NULL,
+  `sid` int(10) NOT NULL,
   `attemptDate` date NOT NULL,
   `firstName` varchar(30) NOT NULL,
   `lastName` varchar(30) NOT NULL,
   `attemptNo` int(10) NOT NULL,
   `score` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `attempts`
---
-
-INSERT INTO `attempts` (`id`, `attemptDate`, `firstName`, `lastName`, `attemptNo`, `score`) VALUES
-(1, '2023-05-04', 'Sadit', 'Ridwan', 1, 8);
 
 --
 -- Indexes for dumped tables
@@ -61,7 +55,7 @@ ALTER TABLE `attempts`
 -- AUTO_INCREMENT for table `attempts`
 --
 ALTER TABLE `attempts`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
